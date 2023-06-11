181e0cdd0b0aaf8e22eae58cb28b59063660f614
bhavane <bhavanejaya@gmail.com>
2023-06-11 17:41:44 +0530

    This is sample commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <div className="main-container">
+        
+          <h1 className="mainheading">
+            Super League </h1>
+            <div className="imgContainer">
+            <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="" />
+            <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="" />
+            </div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default App
